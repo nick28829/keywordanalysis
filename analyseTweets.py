35a2cad4d57ad510d0b12cbe1f405c1293bb5cf7
Nicklas Bocksberger <nicklas@bocksberger.com>
2021-11-01 14:06:46 +0100

    add function to analyse a tweet

---
+++ analyseTweets.py
@@ -0,0 +1,46 @@
+from demoji import replace_with_desc
+import re
+import spacy
+
+# some things that need to be done once 
+nlp = spacy.load("de_core_news_md")
+mention = re.compile(r"@\w*")
+link = re.compile(r"https?://[\w\.-/]*")
+
+
+def containsKeyword(tweet: str, keyword: str, sim_single: float, sim_double: float) -> bool:
+    """
+    Process Tweet and tell wheter or not it contains a certain 
+    keyword based an similarity of the word vectors. If it contains
+    two words with a similarity over sim_double % (default: 70) or one word with a similarity 
+    over sim_single % (default: 55) it returns True.
+    """
+
+    # replace some stuff in the tweet
+    tweet = mention.sub(" ", tweet)
+    tweet = link.sub(" ", tweet)
+    tweet = tweet.replace('#', '')
+    tweet = tweet.replace('\n', '')
+    tweet = replace_with_desc(tweet)
+
+    # use spaCy pipeline on tweet & kw to get vectors
+    doc = nlp(tweet)
+    tokens = [w for w in doc if not (w.is_punct or w.is_stop) and w.has_vector]
+    kw_vector = nlp(keyword)
+
+    # if keyword can't be vectorized, raise an Exception
+    if not kw_vector.has_vector:
+        raise KeyError('Keyword %s could not be vectorized', keyword)
+
+    similarities = [keyword.similarity(w) for w in tokens]
+
+    if max(similarities) >= 0.7:
+        return True
+
+    # count elements
+    # bigger_double_sim = sum(s > sim_double for s in similarities)
+    if sum(s > sim_double for s in similarities) > 2:
+        return True
+
+    # Otherwise
+    return False
